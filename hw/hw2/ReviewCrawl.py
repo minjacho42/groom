4cd490b66ac9c4c7360d735956deddb1e6145554
cmj-dev <louis9982.dev@gmail.com>
2022-07-15 18:40:43 +0900

    consider KeyboardInterrupt & change way to print current page

--- hw/hw2/ReviewCrawl.py
+++ hw/hw2/ReviewCrawl.py
@@ -6,7 +6,7 @@ def ReqByPage(page : int):
     for pg in range(page):
         sleep(0.5) # 페이지 조회 시간 차
         response = requests.get(f"https://movie.naver.com/movie/point/af/list.naver?&page={pg+1}") # get request
-        print(f"current page : {pg+1}")
+        print(f"\rcurrent page : {pg+1}", end="")
         soup = BSoup(response.text, 'html.parser')
         body = soup.find(name='tbody')
         for element in body.find_all(name='td', attrs={'class':'title'}):
@@ -23,8 +23,11 @@ def ReviewCrawl():
     with open('samples.csv','w', encoding="utf8") as fd:
         writer = csv.DictWriter(fd, fieldnames=['movie','sentence','score'])
         writer.writeheader()
-        for sample in ReqByPage(1000):
-            writer.writerow(sample)
+        try:
+            for sample in ReqByPage(1000):
+                writer.writerow(sample)
+        except KeyboardInterrupt:
+            print("\nKeyboard Interrupted End Crawling")
 
 if __name__ == '__main__':
     ReviewCrawl()
