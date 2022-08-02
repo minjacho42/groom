6c048aedc0029fed3bf1307b3194580c845bae78
cmj-dev <louis9982.dev@gmail.com>
2022-08-02 15:30:20 +0900

    mv leet* to leet dir

--- algo/leet977.py
+++
@@ -1,7 +0,0 @@
-
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        result = []
-        for num in sorted(nums, key=lambda x : x**2):
-            result.append(num**2)
-        return result
